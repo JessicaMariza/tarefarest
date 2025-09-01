b92bab0e09ddec41b2f577c3954bcdf677536753
Jessica Mariza <jessicamariza.sz@gmail.com>
2025-09-01 00:18:09 -0300

    Trabalho

---
+++ src/test/java/com/example/demotarefarest/DemotarefarestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotarefarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotarefarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
